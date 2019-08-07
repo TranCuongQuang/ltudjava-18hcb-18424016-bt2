90ed55de566e453f40f7d059cefb34b6e33d6365
Tran Quang Cuong <cuongtq@s-wifi.vn>
2019-08-07 22:01:43 +0700

    create project

---
+++ ltudjava18hcb18424016bt2/src/ltudjava18hcb18424016bt2/Ltudjava18hcb18424016bt2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ltudjava18hcb18424016bt2;
+
+/**
+ *
+ * @author quang
+ */
+public class Ltudjava18hcb18424016bt2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
