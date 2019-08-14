4b7abd7f158b82f76047b88c035191dbb0cb9609
Tran Quang Cuong <cuongtq@s-wifi.vn>
2019-08-15 00:28:14 +0700

    import student in class

--- ltudjava18hcb18424016bt2/src/ltudjava18hcb18424016bt2/Class.java
+++ ltudjava18hcb18424016bt2/src/ltudjava18hcb18424016bt2/Class.java
@@ -5,21 +5,95 @@
  */
 package ltudjava18hcb18424016bt2;
 
+import DAO.StudentDAO;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.TitledBorder;
+import pojos.Student;
 
 /**
  *
  * @author quang
  */
-public class Class {
+public class Class extends JPanel implements ActionListener {
+
+    JFileChooser fc;
+    JButton openButton;
+    JPanel pnManClass;
 
     public JPanel CreateLayout() {
-        JPanel pnManClass = new JPanel();
+        pnManClass = new JPanel();
         TitledBorder titleClass = new TitledBorder("Quản lý lớp học");
         pnManClass.setBorder(titleClass);
         pnManClass.add(new JTextField("TextField", 20));
+
+        fc = new JFileChooser();
+        openButton = new JButton("Import");
+        pnManClass.add(openButton);
+        openButton.addActionListener(this);
         return pnManClass;
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == openButton) {
+            int returnVal = fc.showOpenDialog(Class.this);
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                File file = fc.getSelectedFile();
+                //JOptionPane.showMessageDialog(null, file.getName() + " Success " + file.getAbsolutePath());
+                ReadData(file.getAbsolutePath(), "Class");
+            }
+        }
+    }
+
+    public static void ReadData(String file, String refix) {
+        BufferedReader br = null;
+        String line = "";
+        String Path = file;
+        try {
+            br = new BufferedReader(new InputStreamReader(new FileInputStream(Path), "UTF-8"));
+            line = br.readLine();
+            while ((line = br.readLine()) != null) {
+                String[] item = line.split(",");
+                if (refix.equals("Class")) {
+                    // Thêm sinh viên
+                    Student st = new Student();
+                    st.setStudentCode(item[0]);
+                    st.setFullName(item[1]);
+                    st.setGender(true);
+                    st.setCardNumber(item[3]);
+                    boolean kq = StudentDAO.SaveStudent(st);
+                    if (kq == true) {
+                        JOptionPane.showMessageDialog(null, "Thêm thành công.");
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Thêm thất bại.");
+                    }
+
+                } else if (refix.equals("TKB")) {
+                    System.out.println("Thông tin thời khóa biểu: [Mã môn học: " + item[0] + " , Tên môn học: " + item[1] + " , Phòng: " + item[2] + "]");
+                }
+            }
+        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                }
+            } else {
+                JOptionPane.showMessageDialog(null, "File không tồn tại.");
+            }
+        }
     }
 }
--- ltudjava18hcb18424016bt2/src/ltudjava18hcb18424016bt2/test.java
+++ ltudjava18hcb18424016bt2/src/ltudjava18hcb18424016bt2/test.java
@@ -9,53 +9,67 @@ import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Container;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.AbstractAction;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.border.TitledBorder;
 
 /**
  *
  * @author quang
  */
-public class Home extends JFrame {
+public class test extends JFrame {
 
     JMenuItem miClass;
     JMenuItem miSchedule;
     JPanel pnMaster;
 
-    public Home(String Title) {
+    public test(String Title) {
         super(Title);
-        // Menu
         JMenuBar mb = new JMenuBar();
         JMenu m1 = new JMenu("Menu");
         mb.add(m1);
 
         // Quản lý thời lớp học
-        Class cl = new Class();
-        JPanel pnManClass = cl.CreateLayout();
+        //Class clClass = new Class();
+        //JPanel pnManClass = clClass.CreateLayout();
+        JPanel pnManClass = new JPanel();
+        //  TitledBorder titleClass = new TitledBorder("Quản lý lớp học");
+        //   pnManClass.setBorder(titleClass);
+        // pnManClass.add(new JTextField("TextField", 20));
 
         // Quản lý thời khóa biểu
-        Schedule sch = new Schedule();
-        JPanel pnManSchedule = sch.CreateLayout();
+        JPanel pnManSchedule = new JPanel();
+        TitledBorder titleSchedule = new TitledBorder("Quản lý thời khóa biểu");
+        pnManSchedule.setBorder(titleSchedule);
+        pnManSchedule.add(new JButton("Button 1"));
+        pnManSchedule.add(new JButton("Button 2"));
+        pnManSchedule.add(new JButton("Button 3"));
 
         // add vào card chung
         pnMaster = new JPanel(new CardLayout());
         pnMaster.add(pnManClass, "ManClass");
         pnMaster.add(pnManSchedule, "ManSchedule");
 
-        CardLayout card = (CardLayout) (pnMaster.getLayout());
+        CardLayout cl = (CardLayout) (pnMaster.getLayout());
 
         miClass = new JMenuItem(new AbstractAction("Quản lý lớp học") {
             public void actionPerformed(ActionEvent e) {
-                card.show(pnMaster, "ManClass");
+                cl.show(pnMaster, "ManClass");
             }
         });
         miSchedule = new JMenuItem(new AbstractAction("Quản lý thời khóa biểu") {
             public void actionPerformed(ActionEvent e) {
-                card.show(pnMaster, "ManSchedule");
+                cl.show(pnMaster, "ManSchedule");
             }
         });
         m1.add(miClass);
@@ -64,6 +78,5 @@ public class Home extends JFrame {
         Container con = getContentPane();
         con.add(BorderLayout.NORTH, mb);
         con.add(BorderLayout.CENTER, pnMaster);
-
     }
 }
